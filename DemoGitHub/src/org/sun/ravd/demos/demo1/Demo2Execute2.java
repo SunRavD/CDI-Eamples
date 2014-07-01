e9a863b7cae8cf73c31c236b852db45096e60f6e
SunRavD <sunkara.raviprakash.feb14@gmail.com>
2014-07-01 11:57:14 +0530

    Implements

---
+++ DemoGitHub/src/org/sun/ravd/demos/demo1/Demo2Execute2.java
@@ -0,0 +1,17 @@
+package org.sun.ravd.demos.demo1;
+
+import java.io.Serializable;
+
+/**
+ * 
+ * @author sunkara ravi alias SunRavD
+ *
+ */
+public class Demo2Execute2  implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
